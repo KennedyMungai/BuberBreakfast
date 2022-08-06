7f12743f44accae98a31f15eb6e7832c685f4797
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-06 14:31:46 +0300

    Put back the ids in the attributes

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -13,19 +13,19 @@ public class BreakfastsController : ControllerBase
         return Ok(request);
     }
 
-    [HttpGet]
+    [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
         return Ok(id);
     }
 
-    [HttpPut]
+    [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
     {
         return Ok(request);
     }
 
-    [HttpDelete]
+    [HttpDelete("{id:guid")]
     public IActionResult DeleteBreakfast(Guid id)
     {
         return Ok(id);
