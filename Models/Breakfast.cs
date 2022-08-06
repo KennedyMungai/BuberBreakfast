717fd8dd90f66579854ab1632a586b7f5c9f240b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-06 15:14:18 +0300

    Removed all the sets

--- Models/Breakfast.cs
+++ Models/Breakfast.cs
@@ -3,12 +3,12 @@ namespace BuberBreakfast.Models;
 
 public class Breakfast
 {
-    public Guid Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public DateTime StartDateTime { get; set; }
-    public DateTime EndDateTime { get; set; }
-    public DateTime LastModifiedDateTime { get; set; }
-    public List<string> Savoury { get; set; }
-    public List<string> Sweet { get; set; }
+    public Guid Id { get;}
+    public string? Name { get;}
+    public string? Description { get;}
+    public DateTime StartDateTime { get;}
+    public DateTime EndDateTime { get;}
+    public DateTime LastModifiedDateTime { get;}
+    public List<string> Savoury { get;}
+    public List<string> Sweet { get;}
 }
