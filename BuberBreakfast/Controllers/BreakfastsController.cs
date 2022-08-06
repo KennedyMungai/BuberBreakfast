30418b5524194ec1ccaafff983b18b51053b8f48
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-06 14:30:15 +0300

    Updated the HTTP attributes

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -4,28 +4,28 @@ using Microsoft.AspNetCore.Mvc;
 namespace BuberBreakfast.Controllers;
 
 [ApiController]
-[Route("{controller}")]
+[Route("breakfasts")]
 public class BreakfastsController : ControllerBase
 {
-    [HttpPost("/breakfasts")]
+    [HttpPost]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
         return Ok(request);
     }
 
-    [HttpGet("/breakfasts/{id:guid}")]
+    [HttpGet]
     public IActionResult GetBreakfast(Guid id)
     {
         return Ok(id);
     }
 
-    [HttpPut("/breakfasts/{id:guid}")]
+    [HttpPut]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
     {
         return Ok(request);
     }
 
-    [HttpDelete("/breakfasts/{id:guid}")]
+    [HttpDelete]
     public IActionResult DeleteBreakfast(Guid id)
     {
         return Ok(id);
