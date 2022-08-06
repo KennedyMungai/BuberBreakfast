72122020c29b5c5dfda0a8dffd7f2d2e0b295a8e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-06 15:54:43 +0300

    Created the file and added basic logic

---
+++ BuberBreakfast/Services/Breakfasts/BreakfastService.cs
@@ -0,0 +1,11 @@
+using BuberBreakfast.Models;
+
+namespace BuberBreakfast.Services.Breakfasts;
+
+public class BreakfastService : IBreakfastService
+{
+    public void CreateBreakfast(Breakfast breakfast)
+    {
+        throw new NotImplementedException();
+    }
+}
