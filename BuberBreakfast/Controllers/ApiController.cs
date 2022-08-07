c9edef6599fdd55a3ba4517970beaa8ca3c51798
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-07 19:57:56 +0300

    Created the Problem function

--- BuberBreakfast/Controllers/ApiController.cs
+++ BuberBreakfast/Controllers/ApiController.cs
@@ -1,3 +1,4 @@
+using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberBreakfast.Controllers;
@@ -6,5 +7,18 @@ namespace BuberBreakfast.Controllers;
 [Route("[controller]")]
 public class ApiController : ControllerBase
 {
-    
+    protected IActionResult Problem(List<Error> errors)
+    {
+        var firstError = errors[0];
+
+        var statusCode = firstError.Type switch
+        {
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+                _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, title: firstError.Description);
+    }
 }
