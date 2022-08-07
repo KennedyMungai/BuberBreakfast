ace696961069f3ddaa8b8927faaae604dbae71b0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-07 20:06:55 +0300

    Changed the return type of the DeleteBreakfast function to ErrorOr of type Deleted

--- BuberBreakfast/Services/Breakfasts/IBreakfastService.cs
+++ BuberBreakfast/Services/Breakfasts/IBreakfastService.cs
@@ -8,5 +8,5 @@ public interface IBreakfastService
     ErrorOr<Created> CreateBreakfast(Breakfast breakfast);
     ErrorOr<Breakfast> GetBreakfast(Guid id);
     ErrorOr<Updated> UpsertBreakfast(Breakfast breakfast);
-    void DeleteBreakfast(Guid id);
+    ErrorOr<Deleted> DeleteBreakfast(Guid id);
 }
